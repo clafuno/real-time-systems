a46b1feb759e24a80246eb1976d1ae56346a5aee
clafuno <clarafurionov@gmail.com>
2016-12-18 13:37:44 +0100

    First draft BF

--- BinPacking.py
+++ BinPacking.py
@@ -113,6 +113,32 @@ def binAddBF(item, peso):
     #print "addBF", item, peso
     global Pesos
     allocated = 0
+    Pesos = []
+    
+
+    #sortedBins = sorted(Bins, key=lambda peso:peso[1])
+    #Count occurrences of max value
+    for i in range(NBins):
+        Pesos.append(Bins[i][1])
+
+    sortedBins = sorted(Bins, key=lambda peso:peso[1])
+
+    if Pesos.count(max(Pesos)) != 1: #¿?¿?
+        i = Pesos.count(max(Pesos))
+        j =  NBins - i #Index of max with lower index
+        #check if it fits
+        if (sortedBins[j][1] + peso) > 1:
+            sortedBins[0][2].append(item)
+            sortedBins[0][1] += peso
+            allocated = 1
+        else:
+            sortedBins[j][2].append(item)
+            sortedBins[j][1] += peso
+            allocated = 1
+    else:
+        sortedBins[-1][2].append(item)
+        sortedBins[-1][1] += peso
+        allocated = 1
     # check "occupation level" of cores
     # Assign to the most occupied one
     # - IF same occupation --> Assign to lower index
